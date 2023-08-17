dafb90e77a930f949ca84ba1efad16efbbac4fbb
Alabidun Mubarak <oluwadamilaremubarak@gmail.com>
2023-08-17 18:09:33 +0100

    Update default text color
    changed

--- lib/skeleton_animation.dart
+++ lib/skeleton_animation.dart
@@ -136,7 +136,7 @@ class _SkeletonState extends State<Skeleton>
   Widget build(BuildContext context) {
     // Get the correct text color and calculate the correct opcity
     Color _themeTextColor =
-        Theme.of(context).textTheme.bodyLarge?.color ?? const Color(0xfff6f6f6);
+        Theme.of(context).textTheme.bodyLarge?.color ?? const Color(0xffdedede);
     double _themeOpacity =
         Theme.of(context).brightness == Brightness.light ? 0.11 : 0.13;
 
